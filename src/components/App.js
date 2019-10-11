9fbd38207e9f52c90b540f816ca71d79952822ef
Jeffrey Ilar <jilar@petzent.com>
2019-10-11 15:20:37 -0700

    Added Video Detail component and video embedding

--- src/components/App.js
+++ src/components/App.js
@@ -5,25 +5,25 @@ import VideoList from './VideoList'
 import VideoDetail from './VideoDetail'
 
 class App extends React.Component{
-  state={videos:[], selectedVideo:null};
+  state={videos:[], selectedVideo : null};
 
   onSearchSubmit= async (term)=>{
     const response = await youtube.get('/search',
     {
       params:{q:term}
     });
-
     this.setState({videos: response.data.items});
   };
 
   onVideoSelect= (video) =>{
-    console.log(video);
+    this.setState({selectedVideo:video});
   };
 
   render(){
     return (
       <div className = "ui container" >
         <SearchBar onSubmit ={this.onSearchSubmit}/>
+        <VideoDetail video={this.state.selectedVideo}/>
         <VideoList videos={this.state.videos} onVideoSelect={this.onVideoSelect}/>
       </div>
     );
